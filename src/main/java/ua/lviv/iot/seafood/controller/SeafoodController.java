ce3cd6d0bdfc1dabf9c2b706568d3188eaf45889
PeeckDann <56189628+PeeckDann@users.noreply.github.com>
2020-04-11 19:58:16 +0300

    Added eighth lab

--- src/main/java/ua/lviv/iot/seafood/controller/SeafoodController.java
+++ src/main/java/ua/lviv/iot/seafood/controller/SeafoodController.java
@@ -1,56 +1,59 @@
 package ua.lviv.iot.seafood.controller;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import ua.lviv.iot.seafood.business.SeafoodService;
 import ua.lviv.iot.seafood.model.Seafood;
 
 @RequestMapping("/seafood")
 @RestController
 public class SeafoodController {
 
-    private static Map<Integer, Seafood> seafoodStorage = new HashMap<>();
-
     private static AtomicInteger idCounter = new AtomicInteger();
+
+    @Autowired
+    private SeafoodService seafoodService;
 
     @GetMapping
     public List<Seafood> getSeafood() {
-        return new LinkedList<Seafood>(seafoodStorage.values());
+        return new LinkedList<Seafood>(seafoodService.findSeafood());
     }
 
     @GetMapping("/{id}")
     public Seafood getPieceOfSeafood(final @PathVariable("id") Integer seafoodId) {
-        return seafoodStorage.get(seafoodId);
+        return seafoodService.findPieceOfSeafood(seafoodId);
     }
 
     @PutMapping(path = "/{id}")
     public ResponseEntity<Object> putSeafood(final @PathVariable("id") Integer seafoodId,
             final @RequestBody Seafood seafood) {
         seafood.setId(seafoodId);
-        if (seafoodStorage.containsKey(seafoodId)) {
-            return ResponseEntity.ok(seafoodStorage.put(seafoodId, seafood));
+        if (seafoodService.checkForSeafoodExistence(seafoodId)) {
+            return ResponseEntity.ok(seafoodService.updateSeafood(seafoodId, seafood));
         }
         return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping(path = { "/{id}" })
     public ResponseEntity<Seafood> deleteSeafood(final @PathVariable("id") Integer seafoodId) {
-        HttpStatus status = seafoodStorage.remove(seafoodId) == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
-        return ResponseEntity.status(status).build();
+        if (seafoodService.checkForSeafoodExistence(seafoodId)) {
+            seafoodService.deleteSeafood(seafoodId);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
     public Seafood createSeafood(final @RequestBody Seafood seafood) {
         seafood.setId(idCounter.incrementAndGet());
-        seafoodStorage.put(seafood.getId(), seafood);
+        seafoodService.createSeafood(seafood);
         return seafood;
     }
 }
